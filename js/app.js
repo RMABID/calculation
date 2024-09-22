cfc7293652971613fad916dcb763f85681a21bb1
RMABID <mdabidaliimran@gmail.com>
2024-09-22 13:23:17 +0600

    history commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
+
+let count = 0;
+
 const calculateAmount = document.getElementById('calculate-amount');
 
 calculateAmount.addEventListener('click', function(){
+    count += 1;
 
     const incomeAmount = parseFloat(document.getElementById('income-input').value);
 const softwareAmount = parseFloat(document.getElementById('software-input').value);
@@ -21,6 +25,21 @@ const result = document.getElementById('result');
 result.classList.remove('hidden')
 
 
+const historyItem = document.createElement('div');
+historyItem.className = 'bg-white p-3 rounded-md border-l-2 border-indigo-500';
+
+historyItem.innerHTML = `
+    <p>Serial : ${count}</p>
+    <p class="text-xs text-gray-500 ">${new Date().toLocaleDateString()}</p>
+    <p class="text-xs text-gray-500 >Income: $${incomeAmount.toFixed(2)}</p>
+    <p class="text-xs text-gray-500 >Expenses: $${totalCost.toFixed(2)}</p>
+    <p class="text-xs text-gray-500 >Balance: $${balance.toFixed(2)}</p>
+`
+
+
+    const historyContainer = document.getElementById('history-section');
+    historyContainer.insertBefore(historyItem, historyContainer.firstChild)
+
 })
 
 const calculateSavingsAmount = document.getElementById('savings-calculate');
@@ -39,12 +58,39 @@ calculateSavingsAmount.addEventListener('click', function(){
     const savingsAmount = (savingsAmountEl * balance) / 100 ;
     
     const savings = document.getElementById('savings-amo');
-    savings.innerText = savingsAmount;
+    savings.innerText = savingsAmount.toFixed(2);
 
 
     const remaingAmount = balance - savingsAmount;
 
     const remaingAmountEl = document.getElementById('remaing-balance');
-    remaingAmountEl.innerText = remaingAmount;
+    remaingAmountEl.innerText = remaingAmount.toFixed(3);
     
+})
+
+
+const historyTab = document.getElementById('history-tab');
+const assistantTab = document.getElementById('assistant-tab');
+historyTab.addEventListener('click', function(){
+
+    historyTab.classList.add(
+        'text-white', 
+        'font-semibold',
+         'bg-gradient-to-r',
+          'from-blue-500', 
+          'to-purple-600'
+        );
+
+
+        assistantTab.classList.remove(
+            'text-white', 
+        'font-semibold',
+         'bg-gradient-to-r',
+          'from-blue-500', 
+          'to-purple-600'
+        );
+
+        document.getElementById('assistant-hidden').classList.add('hidden')
+        document.getElementById('history-section').classList.remove('hidden')
+        
 })
